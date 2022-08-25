a11b2aad97a1f5422795bc4ab16404571085ae34
VictorCaique <vcds4747@gmail.com>
2022-08-24 21:08:45 -0300

    Setting up database

--- aepfatec/aep/admin.py
+++ aepfatec/aep/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import ItemPerdido, Requesicao
+
+admin.site.register(ItemPerdido)
+admin.site.register(Requesicao)
+
 # Register your models here.
--- aepfatec/aep/models.py
+++ aepfatec/aep/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+class ItemPerdido(models.Model):
+    descricao = models.fields.TextField(max_length=1000)
+    data_encontrado = models.fields.DateField()
+    image_uri = models.fields.CharField(max_length=500)
+    titulo = models.fields.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.descricao
+
+class Requesicao(models.Model):
+    nome = models.fields.CharField(max_length=255)
+    email = models.fields.EmailField()
+    telefone = models.fields.CharField(max_length=12)
+    data_perdido = models.fields.DateField()
+    mensagem = models.fields.TextField(max_length=2000)
+
 # Create your models here.
